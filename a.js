f458386b6d2c08ef963f3c2f09cd2e7a2fd1c20a
zyad_ahmed_khalifa <zyad.ah2352006@gmail.com>
2025-04-05 19:58:50 +0300

    js assignment 1

---
+++ a.js
@@ -0,0 +1,72 @@
+// section 1: basics & operators
+
+// 1
+// let a = 10;
+// let b = 5;
+// let result = a + b;
+// console.log(result)
+
+// 2
+// let a = 15 % 4
+// console.log(a)
+
+// 3
+// let a = "zyad"
+// console.log(a+3)
+
+//------------------------------------------------------------------------------
+
+// section 2: conditions & loops
+
+// 1
+// let a = 3
+// if (a % 2 == 0) {
+//     console.log("even")
+// } else {
+//     console.log("odd")
+// }
+
+// 2
+// let a = 15
+// if (a >= 10 && a <= 50) {
+//     console.log("yes")
+// } else {
+//     console.log("no")
+// }
+
+// 3
+// for (let i = 1; i <= 10; i++){
+//     if (i == 5) continue
+//     console.log(i)
+// }
+
+//------------------------------------------------------------------------------
+
+// section 3: functions & scopes
+
+// 1
+// function greeating(a){
+//     console.log(`hello, ${a}`)
+// }
+// greeating("zyad");
+
+// 2
+// function squar(a) {
+//     a *= a
+//     console.log(a) // console.log(a*a)
+// }
+// squar(5)
+
+// 3
+// function add(a, b = 10) {
+//     console.log(a + b)
+// }
+// add(3)
+
+//------------------------------------------------------------------------------
+
+// challenge
+
+// 2
+// let a = Math.round(Math.random() * 100 )
+// console.log(a)
